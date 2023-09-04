0de6b5d792cede1c62baae0fbcc930c276e12ed9
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2023-09-04 21:53:50 +0300

    Update axios.ts

--- src/app/axios.ts
+++ src/app/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://api.samuraiconquest.com',
+  baseURL: 'https://api.samuraiconquest.com',
 });
 
 export default instance;
